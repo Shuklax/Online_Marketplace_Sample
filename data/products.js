54c34c2151c95cf51917a4219604068f2407a163
Shuklax <harshshukla@hotmail.com>
2023-07-10 20:20:16 +0530

    Add new image assets to product list

--- data/products.js
+++ data/products.js
@@ -1,18 +1,36 @@
 const products = [
   {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: "images/products/rolling-pin.jpeg",
+    name: "Rolling pin",
     rating: {
       stars: 4.5,
-      count: 87
+      count: 175000
     },
-    priceCents: 1090,
+    priceCents: 0,
     keywords: [
-      "socks",
-      "sports",
-      "apparel"
+      "kitchen",
+      "cookware"
     ]
+  },
+  {
+    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    image: "images/products/Castling-rights.jpeg",
+    name: "Castling Rights",
+    rating: {
+      stars: 4.5,
+      count: 235
+    },
+    priceCents: 2070,
+    keywords: [
+      "chess",
+      "Castle",
+      "King-Rook CheckMate",
+      "King",
+      "Rook"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
   },
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
@@ -29,9 +47,68 @@ const products = [
     ]
   },
   {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: "images/products/desk.jpeg",
+    name: "Office desk",
+    rating: {
+      stars: 5,
+      count: 4
+    },
+    priceCents: 8011,
+    keywords: [
+      "desk",
+      "laziness",
+    ]
+  },
+  {
+    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    image: "images/products/time-wallpaper.jpeg",
+    name: "Clocks",
+    rating: {
+      stars: 5,
+      count: 0
+    },
+    priceCents: 100,
+    keywords: [
+      "time",
+      "money",
+      "Samay"
+    ]
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "images/products/budget-earphone.jpeg",
+    name: "Budget Earphone",
+    rating: {
+      stars: 4.5,
+      count: 150000
+    },
+    priceCents: 90,
+    keywords: [
+      "earphone",
+      "sasta",
+      "cheap"
+    ]
+  },
+  {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
+    name: "Hoodie Shirt",
+    rating: {
+      stars: 4.5,
+      count: 317
+    },
+    priceCents: 2400,
+    keywords: [
+      "hoodies",
+      "sweaters",
+      "apparel"
+    ]
+  },
+  {
     id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    name: "Hoodie",
     rating: {
       stars: 4.5,
       count: 56
@@ -46,61 +123,17 @@ const products = [
     sizeChartLink: "images/clothing-size-chart.png"
   },
   {
-    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-    image: "images/products/black-2-slot-toaster.jpg",
-    name: "2 Slot Toaster - Black",
-    rating: {
-      stars: 5,
-      count: 2197
-    },
-    priceCents: 1899,
-    keywords: [
-      "toaster",
-      "kitchen",
-      "appliances"
-    ]
-  },
-  {
-    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-    image: "images/products/6-piece-white-dinner-plate-set.jpg",
-    name: "6 Piece White Dinner Plate Set",
-    rating: {
-      stars: 4,
-      count: 37
-    },
-    priceCents: 2067,
-    keywords: [
-      "plates",
-      "kitchen",
-      "dining"
-    ]
-  },
-  {
-    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
-    image: "images/products/6-piece-non-stick-baking-set.webp",
-    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Woolen Socks - 6 Pairs",
     rating: {
       stars: 4.5,
-      count: 175
+      count: 87
     },
-    priceCents: 3499,
+    priceCents: 1090,
     keywords: [
-      "kitchen",
-      "cookware"
-    ]
-  },
-  {
-    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
-    image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
-    name: "Plain Hooded Fleece Sweatshirt",
-    rating: {
-      stars: 4.5,
-      count: 317
-    },
-    priceCents: 2400,
-    keywords: [
-      "hoodies",
-      "sweaters",
+      "socks",
+      "sports",
       "apparel"
     ]
   },
@@ -138,7 +171,7 @@ const products = [
   {
     id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
     image: "images/products/knit-athletic-sneakers-gray.jpg",
-    name: "Waterproof Knit Athletic Sneakers - Gray",
+    name: "Gray Snickers",
     rating: {
       stars: 4,
       count: 89
@@ -149,25 +182,6 @@ const products = [
       "running shoes",
       "footwear"
     ]
-  },
-  {
-    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
-    image: "images/products/women-chiffon-beachwear-coverup-black.jpg",
-    name: "Women's Chiffon Beachwear Cover Up - Black",
-    rating: {
-      stars: 4.5,
-      count: 235
-    },
-    priceCents: 2070,
-    keywords: [
-      "robe",
-      "swimsuit",
-      "swimming",
-      "bathing",
-      "apparel"
-    ],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png"
   },
   {
     id: "aad29d11-ea98-41ee-9285-b916638cac4a",
@@ -186,7 +200,7 @@ const products = [
   {
     id: "04701903-bc79-49c6-bc11-1af7e3651358",
     image: "images/products/women-beach-sandals.jpg",
-    name: "Women's Two Strap Buckle Sandals - Tan",
+    name: "Highly Uncomfortable sandals - Women Special",
     rating: {
       stars: 4.5,
       count: 562
@@ -218,7 +232,7 @@ const products = [
   {
     id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
     image: "images/products/men-slim-fit-summer-shorts-gray.jpg",
-    name: "Men's Slim-Fit Summer Shorts",
+    name: "Taango ke baal Dikhaane ki Machine",
     rating: {
       stars: 4,
       count: 160
@@ -276,21 +290,6 @@ const products = [
       "straw hats",
       "summer",
       "apparel"
-    ]
-  },
-  {
-    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
-    image: "images/products/sky-flower-stud-earrings.webp",
-    name: "Sterling Silver Sky Flower Stud Earrings",
-    rating: {
-      stars: 4.5,
-      count: 52
-    },
-    priceCents: 1799,
-    keywords: [
-      "jewelry",
-      "accessories",
-      "womens"
     ]
   },
   {
@@ -658,62 +657,3 @@ const products = [
     ]
   }
 ];
-
-// let productsHTML = "";
-
-// products.forEach((product) => {
-//   productsHTML += `
-//     <div class="product-container">
-//     <div class="product-image-container">
-//       <img class="product-image"
-//         src="${product.image}">
-//     </div>
-
-//     <div class="product-name limit-text-to-2-lines">
-//       ${product.name}
-//     </div>
-
-//     <div class="product-rating-container">
-//       <img class="product-rating-stars"
-//         src="images/ratings/rating-${product.rating.stars * 10}.png">
-//       <div class="product-rating-count link-primary">
-//         ${product.rating.count}
-//       </div>
-//     </div>
-
-//     <div class="product-price">
-//       ${(product.priceCents / 100).toFixed(2)}
-//     </div>
-
-//     <div class="product-quantity-container">
-//       <select>
-//         <option selected value="1">1</option>
-//         <option value="2">2</option>
-//         <option value="3">3</option>
-//         <option value="4">4</option>
-//         <option value="5">5</option>
-//         <option value="6">6</option>
-//         <option value="7">7</option>
-//         <option value="8">8</option>
-//         <option value="9">9</option>
-//         <option value="10">10</option>
-//       </select>
-//     </div>
-
-//     <div class="product-spacer"></div>
-
-//     <div class="added-to-cart">
-//       <img src="images/icons/checkmark.png">
-//       Added
-//     </div>
-
-//     <button class="add-to-cart-button button-primary">
-//       Add to Cart
-//     </button>
-//   </div>
-//     `;
-// });
-
-// console.log(productsHTML);
-
-// document.querySelector(".js-products-grid").innerHTML = productsHTML;
